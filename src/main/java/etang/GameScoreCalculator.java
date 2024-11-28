c295096f2a1875fd41a7c5d2ba45519ed5292c2b
Ethan <ethanfrancois0@gmail.com>
2024-11-28 11:47:02 +0100

    yams case

--- src/main/java/etang/GameScoreCalculator.java
+++ src/main/java/etang/GameScoreCalculator.java
@@ -1,4 +1,13 @@
 package etang;
 
+import java.util.List;
+
 public class GameScoreCalculator {
+
+    public static int calculateScore(List<Integer> diceRoll) {
+
+        if (diceRoll.stream().distinct().count() == 1) return 50;
+        
+        return 0;
+    }
 }
--- src/test/java/etang/GameScoreCalculatorTest.java
+++ src/test/java/etang/GameScoreCalculatorTest.java
@@ -1,4 +1,36 @@
 package etang;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class GameScoreCalculatorTest {
+
+    @Test
+    public void shouldReturnYamsScore() {
+
+        // Given
+        List<Integer> diceRoll = List.of(6, 6, 6, 6, 6);
+
+        // When
+        int score = GameScoreCalculator.calculateScore(diceRoll);
+
+        // Then
+        assertEquals(50, score);
+
+        // Given
+        diceRoll = List.of(2, 2, 2, 2, 2);
+
+        // When
+        score = GameScoreCalculator.calculateScore(diceRoll);
+
+        // Then
+        assertEquals(50, score);
+    }
+
+    
+
+
 }
